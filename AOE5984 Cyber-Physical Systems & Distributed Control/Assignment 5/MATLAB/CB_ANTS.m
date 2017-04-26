a1029c75cae41e43a4d10095f3f2a50a05c73da9
gkontoudis <g.kontoudis12@gmail.com>
2017-04-26 18:09:11 -0400

    Add Constant Boost CB-ANTS
    Object's valocity and acceleration given

---
+++ AOE5984 Cyber-Physical Systems & Distributed Control/Assignment 5/MATLAB/CB_ANTS.m
@@ -0,0 +1,104 @@
+%% #### -------------------------------------------------------------- #### 
+%% George Kontoudis, Ph.D. Student, gpkont@vt.edu, www.georgekontoudis.com              
+%% Virginia Tech, Department of Mechanical Engineering                         
+%% AOE 5984: Cyber-Physical Systems & Distributed Control      
+%% Project, Cooperaive Mobile Manipulation without Communication 
+%% Constant Boost Force (CB-ANTS) - Assumption: Obect's velocity and acceleration at CoM known
+%% #### -------------------------------------------------------------- #### 
+clear all;
+close all;
+clc;
+%% #### -------------------------------------------------------------- ####
+%Main body
+tic; % start time estimation
+
+M=2; % object mass (kg)
+N=20; % number of robots
+f_f=0.8; % maximum force of each follower (N)
+f_l=0.96; % maximum force of leader (N)
+mu_k=.59; % kinetic friction
+g=9.81; % acceleration of gravity (m/s^2)
+v_t_norm=.3; % maximum velocity value (m/s)
+
+K_p=6; % gain limit to guarantee stability = 10
+
+v_dmax=.3;
+v_dx=[-.2*ones(25,1); .2*ones(25,1); .2*ones(25,1); -.2*ones(25,1) ]; % desired velocity x-axis
+v_dy=[.2*ones(25,1); .2*ones(25,1); -.2*ones(25,1); -.2*ones(25,1)]; % desired velocity y-axis
+
+v_tx=rand(20)*v_dmax; % random initial values x-axis, max(v_t)=.3
+v_ty=rand(20)*v_dmax; % random initial values y-axis, max(v_t)=.3
+
+Dt_max=(N*abs(v_dmax))/(mu_k*g); % Theorem 1 maximum time step
+Dt=.3; % value of time step
+
+K=100; % number of time steps
+Td=1:100;
+td=Dt*Td;
+T=1:101;
+t=Dt*T;
+
+for i=1:K
+    for j=1:N    
+    Bx(i,j)=(K_p*(abs(v_dx(i))-abs(v_tx(i,j)))*Dt)/(M*abs(v_dx(i)));
+    By(i,j)=(K_p*(abs(v_dy(i))-abs(v_ty(i,j)))*Dt)/(M*abs(v_dy(i)));
+    Ax(i,j)=(1-((mu_k*g*Dt)/(N*abs(v_tx(i,j)))));
+    Ay(i,j)=(1-((mu_k*g*Dt)/(N*abs(v_ty(i,j)))));
+    F_fx(i,j)=(mu_k*M*g*v_tx(i,j))/(N*abs(v_tx(i,j)));
+    F_fy(i,j)=(mu_k*M*g*v_ty(i,j))/(N*abs(v_ty(i,j)));
+    v_tx(i+1,j)=Ax(i,j)*v_tx(i,j)+Bx(i,j)*v_dx(i);
+    v_ty(i+1,j)=Ay(i,j)*v_ty(i,j)+By(i,j)*v_dy(i);
+    end
+v_tx_mean= mean(v_tx');
+v_ty_mean= mean(v_ty');
+F_lx(i)=(K_p*(abs(v_dx(i))-abs(v_tx_mean(i)))*v_dx(i))/abs(v_dx(i));
+F_ly(i)=(K_p*(abs(v_dy(i))-abs(v_ty_mean(i)))*v_dy(i))/abs(v_dy(i));
+end
+
+toc; % end time estimation
+
+%% #### -------------------------------------------------------------- ####
+% Plots
+figure (1)
+for q=1:20
+plot(t,v_tx(:,q))
+hold on
+end
+plot(td, v_dx, ':bs')
+hold on
+grid on
+ylabel('V_x [m/s]')
+xlabel('t [s]')
+
+figure (2)
+for q=1:20
+plot(t,v_ty(:,q))
+hold on
+end
+plot(td, v_dy, ':bs')
+hold on
+grid on
+ylabel('V_y [m/s]')
+xlabel('t [s]')
+
+figure (3)
+for q=1:20
+plot(td,F_fx(:,q))
+hold on
+end
+plot(td, F_lx, ':bs')
+hold on
+grid on
+ylabel('F_x [N]')
+xlabel('t [s]')
+
+figure (4)
+for q=1:20
+plot(td,F_fy(:,q))
+hold on
+end
+plot(td, F_ly, ':bs')
+hold on
+grid on
+ylabel('F_y [N]')
+xlabel('t [s]')
