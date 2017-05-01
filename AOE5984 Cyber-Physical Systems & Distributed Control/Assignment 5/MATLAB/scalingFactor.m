d7107fa2a80e698c2af382100af5fbb882886411
gkontoudis <g.kontoudis12@gmail.com>
2017-04-30 23:58:20 -0400

    Update scaling factor

--- AOE5984 Cyber-Physical Systems & Distributed Control/Assignment 5/MATLAB/scalingFactor.m
+++ AOE5984 Cyber-Physical Systems & Distributed Control/Assignment 5/MATLAB/scalingFactor.m
@@ -2,21 +2,21 @@
 %% George Kontoudis, Ph.D. Student, gpkont@vt.edu, www.georgekontoudis.com              
 %% Virginia Tech, Department of Mechanical Engineering                         
 %% AOE 5984: Cyber-Physical Systems & Distributed Control      
-%% Project, Cooperaive Mobile Manipulation without Communication 
+%% Semester Project, Cooperative Mobile Manipulation without Communication 
 %% Scaling Factor of Disturbances
 %% #### -------------------------------------------------------------- #### 
 clear all;
 close all;
 clc;
 
-N=linspace(4,50,45);
+N=6:200;
 
-for k=1:45
+for k=1:size(N,2)
     sf(k)=(2*N(k))/(N(k)^2-N(k));
 end
 
 figure (1)
 plot (N,sf, '--o')
-axis([4 50 0 .5]);
-xlabel('N');
-ylabel('Scaling Down');
+axis([4 200 0 .5]);
+xlabel('N-robots');
+ylabel('Scaling Disturbing Factor');
